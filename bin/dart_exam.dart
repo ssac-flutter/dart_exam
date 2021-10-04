597a59ae6ce867548af0946bd11e0e88c397f91b
Junsuk Oh <a811219@gmail.com>
2021-10-04 16:49:10 +0900

    성직자 작성

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,6 +1,33 @@
+class Hero {
+  String name;
+  int hp;
+
+  Hero(this.name, this.hp);
+
+  void attack() {
+    print('공격했다');
+  }
+}
+
+class Cleric extends Hero {
+  static final int maxHp = 50;
+  static final int maxMp = 10;
+
+  int mp = maxMp;
+
+  Cleric(String name, int hp) : super(name, hp);
+}
+
+
 void main(List<String> arguments) {
-  print('헬로 세계');
-  print('헬로 세계');
-  print('헬로 세계');
-  print('헬로 세계');
+  final hong = Hero('홍길동', 100);
+  final lim = Hero('임꺽정', 100);
+  final lim2 = Hero('임꺽정', 100);
+
+  print(hong.hashCode);
+  print(lim.hashCode);
+  print(lim2.hashCode);
+
+  final cleric = Cleric('우서', 300);
+
 }
